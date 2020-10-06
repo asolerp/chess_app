d67e388b8d560a542fcb4d282478525b3cf7f1e8
Alberto Soler <asoler@topdoctors.es>
2020-10-06 12:11:16 +0200

    v1 chess_app

---
+++ client/src/components/BoardComponent/BoardComponent.js
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from 'react';
+import {ChessBoard} from "react-fen-chess-board";
+import socketIOClient from "socket.io-client";
+
+import './BoardComponent.css'
+
+// UI
+import { PgnComponent } from '../PgnComponent/PgnComponent'
+
+export const BoardComponent = () => {
+    
+  const ENDPOINT = "http://127.0.0.1:4003";
+
+  const [ tournament, setTournament ] = useState()
+  const [ positions, setPositions ] = useState(undefined)
+  const [ partidas, setPartidas ] = useState(undefined)
+  const [ jugadas, setJugadas ] = useState(undefined)
+  const [ live, setLive ] = useState(undefined)
+  const [ pgns, setPgns ] = useState(undefined)
+
+  const brownBoardTheme = {
+    darkSquare: "#b58863",
+    lightSquare: "#f0d9b5"
+  };
+
+  const initialPosition = ['rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1']
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("get_data", data => {
+      console.log("Llegan datos!", data)
+      setTournament(data)
+    });
+  }, []);
+
+  useEffect(() => {
+    const positions = tournament && tournament.map((game, i) => (0))
+    setPositions(positions)
+  },[tournament])
+
+  useEffect(() => {
+    const lives = tournament && tournament.map((game, i) => ( true))
+    setLive(lives)
+  },[tournament])
+
+  useEffect(() => {
+    let jugadas = tournament && tournament.map((game, i) => (game.match[game.match.length - 1]))
+    setJugadas(jugadas)
+  },[tournament])
+
+  useEffect(() => {
+    let pgns = tournament && tournament.map((game, i) => ([...game.pgns]))
+    setPgns(pgns)
+  },[tournament])
+
+  useEffect(() => {
+    console.log("partidas")
+    let partidas = tournament && tournament.map((game, i) => ([...initialPosition,...game.match]))
+    setPartidas(partidas)
+  },[tournament])
+
+  useEffect(() => {
+    let newPositions = positions && [...positions]
+    live && live.forEach((l, i) => {
+      if (l) {
+        newPositions[i] = partidas[i].length - 1
+      }
+    })
+    setPositions(newPositions)
+  }, [partidas, live])
+
+  const next = (index) => {
+    let newLives = [...live]
+    newLives[index] = false
+    setLive(newLives)
+    if (positions[index] < (partidas[index].length - 1)) {
+      const newPositions = [...positions]
+      newPositions[index] = newPositions[index] + 1
+      setPositions(newPositions)
+    }
+  }
+
+  const back = (index) => {
+    let newLives = [...live]
+    newLives[index] = false
+    setLive(newLives)
+    if (positions[index] > 0) {
+      const newPositions = [...positions]
+      newPositions[index] = newPositions[index] - 1
+      setPositions(newPositions)
+    }
+  }
+
+  const goToPosition = (index, position) => {
+    console.log("[[INDEX]]", index)
+    console.log("[[POSITION]]", position)
+    let newLives = Array.isArray(live) && [...live]
+    newLives[index] = false
+    setLive(newLives)
+
+    let newPositions = [...positions]
+    newPositions[index] = position
+    setPositions(newPositions)
+  }
+ 
+
+  const setLiveHandler = (index) => {
+    let newLives = [...live]
+    newLives[index] = true
+    setLive(newLives)
+    let newPositions = [...positions]
+    newPositions[index] = partidas[index].length - 1
+    setPositions(newPositions)
+  }
+
+  return (
+    <React.Fragment>
+      <div className="board-wrapper">
+        {
+          partidas && partidas.map((game, i) => (
+            <div style={{display: 'flex', padding: '10px'}} key={`game-${i}`}>
+            <div className="board-wrapper__mainboard">
+              <ChessBoard 
+                fen={live && live[i] ? jugadas && jugadas[i] : partidas && partidas[i][positions && positions[i]]}
+                boardTheme={brownBoardTheme}
+              />
+            <div className="board-wrapper__actions">
+              <button onClick={() => back(i)} disabled={partidas && partidas[i].length === 1}>Atrás</button>
+              <button onClick={() => next(i)} disabled={partidas && partidas[i].length === 1}>Siguiente</button>
+              <button onClick={() => setLiveHandler(i)}>Live</button>
+            </div>
+            </div>
+            <div className="bard-wrapper__pgns">
+              {
+              pgns && pgns[i].map((pgn, x) => (
+                <PgnComponent 
+                  key={i + x}
+                  onClick={() => goToPosition(i, x+1)} 
+                  active={x === positions[i] - 1 && 'yellow'}  
+                  pos={x}    
+                  pgn={pgn}    
+                />
+              ))
+              }
+            </div>
+          </div>
+          ))
+        }
+        </div>
+    </React.Fragment>
+  )
+}
---
+++ server/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const express = require("express");
+const http = require("http");
+
+const socketIo = require("socket.io");
+const SftpClient = require('./utils/sftpClient')
+
+const index = require("./routes/index");
+
+
+const event = new SftpClient()
+const app = express();
+
+app.use(index);
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+event.con.on("upload", function (data) {
+  const updatedTournament = data.tournament.map((game, i) => ({
+    ...game, pgns: game.pgns.map((pgn, x) => {
+      return pgn.replace(data.tournament[i].pgns[x - 1], '').trim()
+    })
+  }))
+  io.sockets.emit("get_data", updatedTournament)
+});
+
+event.con.on("error", function (data) {
+	console.log(data.toString())
+});
+
+server.listen(process.env.PORT || 4003, () => console.log(`Listening on port ${process.env.PORT}`));
